060d3876a67ece700238c8d82d9123ef6ba539b6
LkeMitchll <mail@luke.fail>
2017-09-21 14:30:04 +0100

    Remember the site theme
    Stores the user preference as a cookie.

--- src/site.js
+++ src/site.js
@@ -1,13 +1,53 @@
-function invert() {
-  if (document.body.className) {
-    document.body.className = "";
+function setCookie(theme) {
+  var d = new Date();
+  d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000));
+  var expires = `expires=${d.toUTCString()}`;
+
+  document.cookie = `theme=${theme}; ${expires}`;
+}
+
+function getCookie(cname) {
+  var name = `${cname}=`;
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function setTheme(theme) {
+  document.body.className = "";
+  document.body.className += theme;
+  setCookie(theme);
+}
+
+function invertTheme() {
+  if (getCookie("theme") == "dark") {
+    setTheme("light");
   } else {
-    document.body.className += "invert";
+    setTheme("dark");
   }
 }
 
-var target = document.getElementsByTagName('h1')[0]
+function setExistingTheme() {
+  if (getCookie("theme")) {
+    setTheme(getCookie("theme"));
+  }
+}
 
-target.addEventListener('click', function() {
-  invert();
-})
+function toggleTheme() {
+  var target = document.getElementsByTagName('h1')[0]
+
+  target.addEventListener('click', function() {
+    invertTheme();
+  })
+}
+
+setExistingTheme();
+toggleTheme();
