043fa7a6f8671ca21790e546dd5c595ed5a57741
LkeMitchll <mail@luke.fail>
2020-09-25 20:26:38 +0100

    Add colophon page

---
+++ pages/colophon.tsx
@@ -0,0 +1,25 @@
+import { ReactElement } from "react";
+import { ContentAPI } from "services/contentful";
+import { Page } from "services/contentful.types";
+import { Hero } from "compositions";
+import { GetStaticProps } from "next";
+import { RichText } from "components";
+
+export const getStaticProps: GetStaticProps = async ({}) => {
+  const api = new ContentAPI();
+  const page = await api.fetchPage("3TxaAfROQOMMOtj5U6E4qG");
+  return { props: { page } };
+};
+
+interface ColophonProps {
+  page: Page;
+}
+
+export default function Colophon({ page }: ColophonProps): ReactElement {
+  return (
+    <>
+      <Hero title={page.title} intro={page.description} />
+      <RichText source={page.content} />
+    </>
+  );
+}
