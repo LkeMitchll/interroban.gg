4e3c6124a3bf3fbef1b488eef285ce89bdc3ea9b
LkeMitchll <mail@luke.fail>
2020-10-25 11:14:31 +0000

    Posts are fully displayed in RSS feed

--- helpers/rss.ts
+++ helpers/rss.ts
@@ -1,27 +1,31 @@
 import { BlogPost } from "services/contentful.types";
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+import { encodeXML } from "entities";
 
 const generateRssItem = (post: BlogPost): string => `
-   <item>
-     <guid>https://interroban.gg/post/${post.slug}</guid>
-     <title>${post.title}</title>
-     <link>https://interroban.gg/post/${post.slug}</link>
-     <description>${post.description}</description>
-     <pubDate>${new Date(post.date).toUTCString()}</pubDate>
-   </item>
- `;
+  <item>
+    <guid>https://interroban.gg/post/${post.slug}</guid>
+    <title>${encodeXML(post.title)}</title>
+    <link>http://interroban.gg/post/${post.slug}</link>
+    <description>
+      <![CDATA[${documentToHtmlString(post.content)}]]>
+    </description>
+    <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+  </item>
+`;
 
 const generateRss = (posts: BlogPost[]): string => `
-   <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-     <channel>
-       <title>Journal - Luke Mitchell</title>
-       <link>https://www.interroban.gg/journal</link>
-       <description>Personal blog of Luke Mitchell, Product Designer</description>
-       <language>en</language>
-       <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
-       <atom:link href="https://interroban.gg/rss.xml" rel="self" type="application/rss+xml"/>
-       ${posts.map(generateRssItem).join("")}
-     </channel>
-   </rss>
- `;
+  <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+    <channel>
+      <title>Journal - Luke Mitchell</title>
+      <link>https://www.interroban.gg/journal</link>
+      <description>Personal blog of Luke Mitchell, Product Designer</description>
+      <language>en</language>
+      <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
+      <atom:link href="https://interroban.gg/rss.xml" rel="self" type="application/rss+xml"/>
+      ${posts.map(generateRssItem).join("")}
+    </channel>
+  </rss>
+`;
 
 export default generateRss;
