d5d60a47070d9572e2ee35d611d7741fabdab87a
LkeMitchll <mail@luke.fail>
2020-11-10 21:32:54 +0000

    Subset font files to reduce download size

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -27,16 +27,9 @@ export default function Layout({
         <link rel="preconnect" href="https://images.ctfassets.net" />
         <link
           rel="preload"
-          href="fonts/pitch-web-semibold-italic.woff2"
+          href="fonts/pitch-web-semibold-italic.subset.woff2"
           as="font"
           type="font/woff2"
-          crossOrigin=""
-        />
-        <link
-          rel="preload"
-          href="fonts/pitch-web-semibold-italic.woff"
-          as="font"
-          type="font/woff"
           crossOrigin=""
         />
       </Head>
