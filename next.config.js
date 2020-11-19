ff2746e2c90cb384f69c8b10f5064681ceaa7414
LkeMitchll <mail@luke.fail>
2020-11-19 21:14:37 +0000

    Add more responsive image sizes

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   },
   poweredByHeader: false,
   images: {
-    deviceSizes: [400, 600, 800, 1000, 1600, 2000],
+    deviceSizes: [400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000],
     domains: ["images.ctfassets.net"],
   },
 };
