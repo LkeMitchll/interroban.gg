b6b0daed6272bc4daa958270a984dafd086f9637
LkeMitchll <mail@luke.fail>
2020-11-26 20:45:10 +0000

    Make button styles more button-y

--- components/designSystem/Button.tsx
+++ components/designSystem/Button.tsx
@@ -2,22 +2,25 @@ import { styled } from "./stitches";
 
 const Button = styled("button", {
   color: "$primary",
-  fontFamily: "$serif",
-  fontSize: "$1",
-  textDecoration: "underline",
-  border: "none",
-  padding: "$0",
+  fontFamily: "$mono",
+  fontSize: "$3",
+  border: "$1 solid $primary",
+  padding: "$_5 $1",
   margin: "$0",
   overflow: "visible",
   background: "transparent",
   WebkitAppearance: "none",
+  transition: "background-color 0.5s ease, color 0.5s ease",
 
   ":hover": {
     cursor: "pointer",
+    backgroundColor: "$primary",
+    color: "$bg",
   },
 
   ":disabled": {
     color: "$faded",
+    backgroundColor: "$bg",
     cursor: "not-allowed",
   },
 });
