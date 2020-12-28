04f646e19b5195da89173740a4bf5534f4c3f3e9
LkeMitchll <mail@luke.fail>
2020-12-28 15:04:55 +0000

    Integration test homepage recent posts & bookmarks

--- cypress/integration/home_page_spec.ts
+++ cypress/integration/home_page_spec.ts
@@ -1,3 +1,5 @@
+import type { BlogPost, Bookmark } from "../../services/contentful.types";
+
 describe("Homepage", () => {
   beforeEach(() => {
     cy.visit("/");
@@ -5,5 +7,27 @@ describe("Homepage", () => {
 
   it("should display the intro section", () => {
     cy.get("h2").should("contain.text", "About Me");
+  });
+
+  it("should display recent posts", () => {
+    let recentPosts: BlogPost[];
+
+    cy.request("GET", "/api/posts").then((response) => {
+      recentPosts = response.body.slice(0, 3);
+      recentPosts.forEach((post) => {
+        cy.get("a").should("contain.text", post.title);
+      });
+    });
+  });
+
+  it("should display recent bookmarks", () => {
+    let recentBookmarks: Bookmark[];
+
+    cy.request("GET", "/api/bookmarks").then((response) => {
+      recentBookmarks = response.body.slice(0, 3);
+      recentBookmarks.forEach((bookmark) => {
+        cy.get("a").should("contain.text", bookmark.title);
+      });
+    });
   });
 });
