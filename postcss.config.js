87b2cd7da4bc93034c50ebc476c167ed531a5412
LkeMitchll <mail@luke.fail>
2021-02-03 11:07:50 +0000

    Split up css using postcss-import

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [require("autoprefixer"), require("cssnano")],
+  plugins: [
+    require("postcss-import"),
+    require("autoprefixer"),
+    require("cssnano"),
+  ],
 };
