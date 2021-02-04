7c5f13d5b1fb9ef97df239326c98c229985a14bb
LkeMitchll <mail@luke.fail>
2021-02-04 16:00:36 +0000

    Tidy up journal & style tracks

--- filters/date.js
+++ filters/date.js
@@ -1,8 +1,8 @@
 module.exports = function formattedDate(date, options) {
   const input = new Date(date);
-  const niceDate = new Intl.DateTimeFormat("en-US", {
-    month: "long",
-    year: "numeric",
-  }).format(input);
+  const niceDate = new Intl.DateTimeFormat(
+    "en-US",
+    options ? options : { month: "long", year: "numeric" }
+  ).format(input);
   return niceDate;
 };
