8db9efe4db2c5f10a7340b431a46080475822aee
LkeMitchll <mail@luke.fail>
2021-02-08 09:53:05 +0000

    Move shortcode function to a seperate file

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,8 @@
 require("dotenv").config();
 
-var unified = require("unified");
-var markdown = require("remark-parse");
-var footnotes = require("remark-footnotes");
-var customFootnotes = require("./helpers/footnotes");
-var html = require("remark-html");
-var all = require("mdast-util-to-hast/lib/all");
-
 module.exports = function (eleventyConfig) {
+  const config = { htmlTemplateEngine: "njk" };
+
   eleventyConfig.addPassthroughCopy("assets/fonts");
   eleventyConfig.addWatchTarget("assets/**/*.css");
 
@@ -15,34 +10,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("limit", require("./filters/limit"));
   eleventyConfig.addFilter("formatDate", require("./filters/date"));
 
-  eleventyConfig.addShortcode("renderMarkdown", (rawMarkdown) => {
-    let result;
-    unified()
-      .use(markdown)
-      .use(footnotes)
-      .use(customFootnotes)
-      .use(html, {
-        handlers: {
-          sectionWithFootnotes: (h, node) => {
-            return h(node, "section", all(h, node));
-          },
-          customFootnoteDefinition: (h, node) => {
-            return h(
-              node,
-              "aside",
-              { id: "fn-" + node.identifier },
-              all(h, node)
-            );
-          },
-        },
-      })
-      .process(rawMarkdown, function (err, output) {
-        if (err) throw err;
-        result = output.contents;
-      });
+  eleventyConfig.addShortcode(
+    "renderMarkdown",
+    require("./shortcodes/markdown")
+  );
 
-    return result;
-  });
-
-  return { htmlTemplateEngine: "njk" };
+  return config;
 };
---
+++ shortcodes/markdown.js
@@ -0,0 +1,35 @@
+var unified = require("unified");
+var markdown = require("remark-parse");
+var footnotes = require("remark-footnotes");
+var customFootnotes = require("../helpers/footnotes");
+var html = require("remark-html");
+var all = require("mdast-util-to-hast/lib/all");
+
+module.exports = function (rawMarkdown) {
+  let result;
+  unified()
+    .use(markdown)
+    .use(footnotes)
+    .use(customFootnotes)
+    .use(html, {
+      handlers: {
+        sectionWithFootnotes: (h, node) => {
+          return h(node, "section", all(h, node));
+        },
+        customFootnoteDefinition: (h, node) => {
+          return h(
+            node,
+            "aside",
+            { id: "fn-" + node.identifier },
+            all(h, node)
+          );
+        },
+      },
+    })
+    .process(rawMarkdown, function (err, output) {
+      if (err) throw err;
+      result = output.contents;
+    });
+
+  return result;
+};
