9d4c55748f762fa125c1d21535ed2653f7f1466e
LkeMitchll <mail@luke.fail>
2021-02-17 10:33:02 +0000

    Add feeds page

---
+++ _data/feeds.js
@@ -0,0 +1,21 @@
+const fetch = require("node-fetch");
+
+module.exports = async function feeds() {
+  const apiEndpoint = "https://api.feedbin.com/v2/";
+  const subscriptionsEndpoint = `${apiEndpoint}subscriptions.json`;
+
+  const credentials = Buffer.from(
+    `${process.env.FEEDBIN_USER}:${process.env.FEEDBIN_PASS}`,
+  ).toString("base64");
+
+  const fetchFeeds = fetch(subscriptionsEndpoint, {
+    headers: {
+      Authorization: `Basic ${credentials}`,
+    },
+  });
+
+  const request = await fetchFeeds;
+  const response = await request.json();
+
+  return response;
+};
