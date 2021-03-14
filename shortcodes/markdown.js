7206dbf9db2a2e2c43575dc3748ebc857b9e9d88
LkeMitchll <mail@luke.fail>
2021-03-14 15:43:15 +0000

    Remove posts TOCs for now

--- shortcodes/markdown.js
+++ shortcodes/markdown.js
@@ -1,13 +1,11 @@
 const unified = require("unified");
 const markdown = require("remark-parse");
 const externalLinks = require("remark-external-links");
-const toc = require("remark-toc");
 const slug = require("remark-slug");
 const footnotes = require("remark-footnotes");
 const html = require("remark-html");
 const all = require("mdast-util-to-hast/lib/all");
 const customFootnotes = require("../helpers/footnotes");
-const customTOC = require("../helpers/toc");
 
 const findByID = require("../filters/findByID");
 const responsiveImage = require("./responsiveImage");
@@ -17,14 +15,11 @@ module.exports = function renderMarkdown(rawMarkdown, assets) {
   unified()
     .use(markdown)
     .use(externalLinks)
-    .use(toc, { ordered: true, maxDepth: 2 })
-    .use(customTOC)
     .use(slug)
     .use(footnotes)
     .use(customFootnotes)
     .use(html, {
       handlers: {
-        TOC: (h, node) => h(node, "nav", all(h, node)),
         sectionWithFootnotes: (h, node) => h(node, "section", all(h, node)),
         customFootnoteDefinition: (h, node) => {
           const id = `fn-${node.identifier}`;
