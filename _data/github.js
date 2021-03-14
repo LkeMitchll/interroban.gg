0824db62756efca3bf29290a0d760390aaecb1a3
LkeMitchll <mail@luke.fail>
2021-03-14 10:59:08 +0000

    Add git commit MVP

---
+++ _data/github.js
@@ -0,0 +1,20 @@
+const { Octokit } = require("@octokit/core");
+
+module.exports = async function commits() {
+  const octokit = new Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
+
+  const response = await octokit.request("GET /repos/{owner}/{repo}/commits", {
+    owner: "lkemitchll",
+    repo: "interroban.gg",
+    per_page: 5,
+  });
+
+  const sanitisedData = response.data.map((child) => ({
+    sha: child.sha.substr(0, 7),
+    message: child.commit.message,
+    url: child.html_url,
+    date: child.commit.committer.date,
+  }));
+
+  return sanitisedData;
+};
