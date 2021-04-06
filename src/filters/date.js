13e93b93cda47f94576572dde37c646c343a1ead
LkeMitchll <mail@luke.fail>
2021-04-06 22:33:28 +0100

    Unify dates

--- src/filters/date.js
+++ src/filters/date.js
@@ -1,8 +1,13 @@
-const formattedDate = (date, options) => {
+const formattedDate = (date, template) => {
   const input = new Date(date);
+  const templates = {
+    long: { month: "long", year: "numeric" },
+    medium: { month: "short", year: "numeric" },
+    short: { day: "numeric", month: "short", year: "numeric" },
+  };
   const niceDate = new Intl.DateTimeFormat(
     "en-US",
-    options || { month: "long", year: "numeric" },
+    templates[template] || templates.short,
   ).format(input);
   return niceDate;
 };
