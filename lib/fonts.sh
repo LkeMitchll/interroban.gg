7aebb3d382caf6a6165a3d813dcf731c8ee31fec
LkeMitchll <mail@luke.fail>
2021-05-03 10:12:42 +0100

    Add Å support in Blanco

--- lib/fonts.sh
+++ lib/fonts.sh
@@ -6,7 +6,7 @@ pyftsubset\
       --flavor=woff2 \
       --layout-features="ccmp,locl,mark,kern,liga,calt,ss01,ss02,smcp,onum"\
       --unicodes="U+0000-00A0,U+00A2-00A9,U+00AC-00AE,U+00B0-00B7,\
-      U+00B9-00BA,U+00BC-00BE,U+00D7,U+00F7,U+2000-206F,U+2074,U+20AC,\
+      U+00B9-00BA,U+00BC-00BE,U+00D7,U+00F7,U+2000-206F,U+2074,U+20AC,U+00C5,\
       U+2122,U+2190-21BB,U+2212,U+2215,U+F8FF,U+FEFF,U+FFFD,U+0250-02AF,U+0100-017F" &&
 
 pyftsubset\
@@ -15,7 +15,7 @@ pyftsubset\
       --flavor=woff \
       --layout-features="ccmp,locl,mark,kern,liga,calt,ss01,ss02,smcp,onum"\
       --unicodes="U+0000-00A0,U+00A2-00A9,U+00AC-00AE,U+00B0-00B7,\
-      U+00B9-00BA,U+00BC-00BE,U+00D7,U+00F7,U+2000-206F,U+2074,U+20AC,\
+      U+00B9-00BA,U+00BC-00BE,U+00D7,U+00F7,U+2000-206F,U+2074,U+20AC,U+00C5,\
       U+2122,U+2190-21BB,U+2212,U+2215,U+F8FF,U+FEFF,U+FFFD,U+0250-02AF,U+0100-017F" &&
 
 pyftsubset\
