ce591b24f182519d96a09c0bb1a73dd78ed9763f
LkeMitchll <mail@luke.fail>
2021-05-05 15:43:00 +0100

    Refactor Savee integration.
    There is no need to call /login endpoint to grab an auth_token before
    calling /items (unless you want to display private items.

--- src/_data/savee.js
+++ src/_data/savee.js
@@ -1,29 +1,15 @@
 import fetch from "node-fetch";
 
 async function saveeFeed() {
-  async function getFeed(token) {
-    return fetch("https://api.savee.it/me/items/", {
+  async function getFeed() {
+    return fetch("https://api.savee.it/user/interrobang/items/", {
       headers: {
-        "Auth-Token": token,
+        "Auth-Token": null,
       },
     }).then((response) => response.json());
   }
 
-  async function auth() {
-    return fetch("https://api.savee.it/auth/login/", {
-      body: `{"username":"${process.env.SAVEE_USER}","password":"${process.env.SAVEE_KEY}"}`,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-    })
-      .then((response) => response.json())
-      .then((json) => json.data.auth_token);
-  }
-
-  return auth()
-    .then((token) => getFeed(token))
-    .then((json) => json.data);
+  return getFeed().then((json) => json.data);
 }
 
 export default saveeFeed;
