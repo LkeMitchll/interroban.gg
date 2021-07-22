d7a553d902c6316834868f26c02f441283525c73
LkeMitchll <mail@luke.fail>
2021-07-22 22:02:02 +0100

    Convert feedbin api calls into a provider

--- _data/feeds.js
+++ _data/feeds.js
@@ -1,24 +1,7 @@
-const Cache = require("@11ty/eleventy-cache-assets");
+const FeedbinAPI = require("../providers/feedbin");
 
 module.exports = async function feeds() {
-  const apiEndpoint = "https://api.feedbin.com/v2/";
-  const subscriptionsEndpoint = `${apiEndpoint}subscriptions.json`;
+  const api = new FeedbinAPI();
 
-  const credentials = Buffer.from(
-    `${process.env.FEEDBIN_USER}:${process.env.FEEDBIN_PASS}`
-  ).toString("base64");
-
-  async function getSubscriptions() {
-    return Cache(subscriptionsEndpoint, {
-      duration: "1d",
-      type: "json",
-      fetchOptions: {
-        headers: {
-          Authorization: `Basic ${credentials}`,
-        },
-      },
-    });
-  }
-
-  return getSubscriptions().then((json) => json);
+  return api.getSubscriptions().then((json) => json);
 };
---
+++ providers/feedbin.js
@@ -0,0 +1,23 @@
+const Cache = require("@11ty/eleventy-cache-assets");
+
+module.exports = class FeedbinAPI {
+  constructor() {
+    this.apiEndpoint = "https://api.feedbin.com/v2/";
+    this.credentials = Buffer.from(
+      `${process.env.FEEDBIN_USER}:${process.env.FEEDBIN_PASS}`
+    ).toString("base64");
+    this.subscriptionsEndpoint = `${this.apiEndpoint}subscriptions.json`;
+  }
+
+  async getSubscriptions() {
+    return Cache(this.subscriptionsEndpoint, {
+      duration: "1d",
+      type: "json",
+      fetchOptions: {
+        headers: {
+          Authorization: `Basic ${this.credentials}`,
+        },
+      },
+    });
+  }
+};
