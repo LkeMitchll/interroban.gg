ec5876fa2645c226020dad6de8ab18dddc78d1f2
LkeMitchll <mail@luke.fail>
2021-08-26 22:10:20 +0100

    Remove some tutorial comments

--- _data/savee.js
+++ _data/savee.js
@@ -3,11 +3,10 @@ const Cache = require("@11ty/eleventy-cache-assets");
 module.exports = async function saveeFeed() {
   const url = "https://api.savee.it/user/interrobang/items/";
 
-  /* This returns a promise */
   async function getFeed() {
     return Cache(url, {
-      duration: "1d", // save for 1 day
-      type: "json", // we’ll parse JSON for you
+      duration: "1d",
+      type: "json",
       fetchOptions: {
         headers: {
           "Auth-Token": null,
