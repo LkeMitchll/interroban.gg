75d6601529dba482563d34c841b45e21a0376660
LkeMitchll <mail@luke.fail>
2021-10-06 21:57:07 +0100

    Remove font script & move date offset helper

--- _data/music.js
+++ _data/music.js
@@ -1,6 +1,15 @@
 const SpotifyAPI = require("../providers/spotify");
 const LastFMAPI = require("../providers/lastfm");
-const dateToEpochWithOffset = require("../helpers/dateEpochOffset");
+
+function dateToEpochWithOffset(time, offset) {
+  const date = new Date();
+  date.setHours(time);
+  date.setMinutes(0);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+  date.setDate(date.getDate() - date.getDay() - offset);
+  return Math.round(date.valueOf() / 1000).toString();
+}
 
 async function lastWeek() {
   const api = new LastFMAPI();
--- helpers/dateEpochOffset.js
+++
@@ -1,9 +0,0 @@
-module.exports = function dateToEpochWithOffset(time, offset) {
-  const date = new Date();
-  date.setHours(time);
-  date.setMinutes(0);
-  date.setSeconds(0);
-  date.setMilliseconds(0);
-  date.setDate(date.getDate() - date.getDay() - offset);
-  return Math.round(date.valueOf() / 1000).toString();
-};
