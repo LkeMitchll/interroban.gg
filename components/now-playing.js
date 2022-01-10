2934caddbdab983807593164659d2451e9b50e90
LkeMitchll <mail@luke.fail>
2022-01-10 23:26:17 +0000

    Refactor now-playing to vanilla HTML/JS

--- components/now-playing.js
+++ components/now-playing.js
@@ -1,50 +1,21 @@
-class NowPlaying extends HTMLElement {
-  constructor() {
-    super();
+async function renderContent() {
+  const title = document.getElementById("now-playing-title");
+  const artist = document.getElementById("now-playing-artist");
 
-    // Get template content from DOM
-    const template = document.getElementById("now-playing");
-    const templateContent = template.content;
-
-    // Create new Shadow Root
-    this.attachShadow({ mode: "open" }).appendChild(
-      templateContent.cloneNode(true)
-    );
-  }
-
-  connectedCallback() {
-    this.getContent();
-  }
-
-  getContent() {
-    // Fetch data from the URL
-    return new Promise((res) => {
-      fetch(this.getAttribute("url"))
-        .then((response) => {
-          if (response.status !== 200) {
-            throw new Error(`HTTP status ${response.status}`);
-          } else {
-            return response.json();
-          }
-        })
-        .then((json) => {
-          this.renderContent(json);
-          res();
-        })
-        .catch(() => null);
-    });
-  }
-
-  renderContent(data) {
-    // Render content into DOM
-    const title = this.shadowRoot.getElementById("title");
-    const artist = this.shadowRoot.getElementById("artist");
-
-    title.setAttribute("href", data.url);
-    title.textContent = data.title;
-
-    artist.textContent = data.artist;
-  }
+  fetch("/.netlify/functions/now-playing")
+    .then((response) => {
+      if (response.status !== 200) {
+        throw new Error(`HTTP status ${response.status}`);
+      } else {
+        return response.json();
+      }
+    })
+    .then((json) => {
+      title.setAttribute("href", json.url);
+      title.textContent = json.title;
+      artist.textContent = json.artist;
+    })
+    .catch(() => null);
 }
 
-customElements.define("now-playing", NowPlaying);
+renderContent();
