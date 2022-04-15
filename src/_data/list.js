6dc8c1a3eac5d11db5214467b6b59e0903862eeb
LkeMitchll <mail@luke.fail>
2022-04-15 11:30:17 +0100

    Convert CV to static JSON

--- src/_data/list.js
+++
@@ -1,7 +0,0 @@
-const contentful = require("../_providers/contentful");
-
-module.exports = async function list() {
-  return contentful.client
-    .getEntries({ content_type: "list" })
-    .then((result) => result.items);
-};
