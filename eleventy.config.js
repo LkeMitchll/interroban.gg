b6fadf4f0229571cd9e11d413ecdbabc85bbf331
LkeMitchll <mail@luke.fail>
2023-02-27 14:42:01 +0000

    Swap esbuild for lightning CSS
    - Adapted from: https://github.com/5t3ph/eleventy-plugin-lightningcss
    - Docs: https://lightningcss.dev/docs.html

--- eleventy.config.js
+++ eleventy.config.js
@@ -11,15 +11,28 @@ module.exports = function config(eleventy) {
 
   // Custom shortcodes
   eleventy.addAsyncShortcode('image', require('./src/_shortcodes/image'));
-
-  const md = require('markdown-it');
   eleventy.addPairedShortcode('sidenote', (content, number) => {
+    const md = require('markdown-it');
     const result = md({ html: true }).render(content);
     return `<aside id="sn-${number}" class="sidenote">
               <small>
                 ${result}
               </small>
             </aside>`;
+  });
+
+  // Recognize CSS as a "template language"
+  eleventy.addTemplateFormats('css');
+  // Process CSS with LightningCSS
+  eleventy.addExtension('css', {
+    outputFileExtension: 'css',
+    compile: async (_, inputPath) => {
+      const { bundle } = require('lightningcss');
+      return async () => {
+        const { code } = bundle({ filename: inputPath, minify: true });
+        return code;
+      };
+    },
   });
 
   // Custom markdown renderer
