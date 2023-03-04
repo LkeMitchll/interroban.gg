eef6ffcdd03d3e92c5a68ec8924a442b97e1dcbd
LkeMitchll <mail@luke.fail>
2023-03-04 12:34:55 +0000

    Add 'notes' to the blog

--- src/_filters/date.js
+++ src/_filters/date.js
@@ -2,7 +2,7 @@ module.exports = function formattedDate(date) {
   const input = new Date(date);
   const niceDate = new Intl.DateTimeFormat(
     'en-US',
-    { day: 'numeric', month: 'short', year: 'numeric' },
+    { day: '2-digit', month: 'short', year: 'numeric' },
   ).format(input);
   return niceDate;
 };
