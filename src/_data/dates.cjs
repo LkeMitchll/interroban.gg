7b5fdaa6ff3d7eb475f5cbd6200b69419de93ecd
LkeMitchll <mail@luke.fail>
2023-09-14 22:44:29 +0100

    Dynamic date to work out how long I've been a designer

--- src/_data/dates.cjs
+++ src/_data/dates.cjs
@@ -3,4 +3,11 @@ module.exports = {
     const today = new Date();
     return today.getFullYear();
   },
+  yearsSinceIBecameADesigner() {
+    const startDate = new Date('2011-09-01');
+    const currentDate = new Date();
+    const timeDiff = Math.abs(currentDate - startDate);
+    const yearsPassed = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 365));
+    return yearsPassed;
+  },
 };
