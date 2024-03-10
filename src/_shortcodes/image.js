47c864e94940aefe4719dc1142676df87ff1c5d3
LkeMitchll <mail@luke.fail>
2024-03-10 11:59:41 +0000

    Tweak images 'sizes' attribute

--- src/_shortcodes/image.js
+++ src/_shortcodes/image.js
@@ -11,11 +11,11 @@ const responsiveImage = async (imgUUID, imgAlt, size = 'large') => {
 
   const viewportWidths = {
     large: {
-      sizes: '(min-width: 800px) 65vw, (min-width: 1600px) 50vw, 90vw',
+      sizes: '(min-width: 1600px) 52.36vw, (min-width: 700px) 67.39vw, 90vw',
       widths: [400, 600, 800, 1000, 1200, 1400],
     },
     small: {
-      sizes: '(min-width: 800px) 14vw, 35vw',
+      sizes: '(min-width: 2000px) 230px, (min-width: 1040px) calc(3.51vw + 161px), (min-width: 600px) calc(30vw - 46px), (min-width: 400px) 308px, calc(72.5vw + 33px)',
       widths: [400, 600],
     },
   };
