7a106731b515b81dfd3097554c9042845d51ec54
LkeMitchll <1729878+LkeMitchll@users.noreply.github.com>
2025-03-06 16:31:21 +0000

    Add deploy datetime to footer

---
+++ src/_data/deploy_info.js
@@ -0,0 +1,29 @@
+import fetch from "@11ty/eleventy-fetch";
+
+function getLatestCommit() {
+  if (process.env.CF_PAGES_COMMIT_SHA) {
+    return process.env.CF_PAGES_COMMIT_SHA.slice(0,7)
+  }
+}
+
+async function latest_deployment_date() {
+  const result = await fetch(
+    process.env.DEPLOY_INFO_URL,
+    {
+      duration: "1d",
+      type: "json",
+      fetchOptions: {
+        headers: {
+          Authorization: `Bearer ${process.env.DEPLOY_INFO_KEY}`,
+        },
+      },
+    },
+  ).then((json) => json);
+
+  return result.result[0].latest_stage.ended_on;
+}
+
+export default {
+  latest_commit: getLatestCommit(),
+  latest_deployment_date: latest_deployment_date()
+}
