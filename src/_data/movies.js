070faff855509fbc9b0b098ef9b5c9fb833d8cd5
Luke Mitchell <LkeMitchll@users.noreply.github.com>
2026-02-13 14:01:32 +0000

    Revert "Replace letterboxd scraping with CF worker"
    This reverts commit d110fc3490577d5535f191a09d0247c2649575c3.

--- src/_data/movies.js
+++ src/_data/movies.js
@@ -2,9 +2,8 @@ import LetterboxdAPI from "../_providers/letterboxd.js";
 
 async function movies() {
   const api = new LetterboxdAPI();
-  const data = await api.getWatches();
-
-  return data;
+  const data = await api.getMovies();
+  return data.slice(0, 6);
 }
 
 export default movies();
--- src/_providers/letterboxd.js
+++ src/_providers/letterboxd.js
@@ -1,21 +1,34 @@
+/* eslint no-underscore-dangle: 0 */
 import Fetch from "@11ty/eleventy-fetch";
+import convert from "xml-js";
 
 export default class LetterboxdAPI {
   constructor() {
-    this.url = `${process.env.LETTERBOXD_URL}/watches?limit=6`;
+    this.apiEndpoint = "https://letterboxd.com/luke_mitchell/rss";
   }
 
-  async getWatches() {
-    return Fetch(this.url, {
+  async getMovies() {
+    const feed = await Fetch(this.apiEndpoint, {
       duration: "1d",
-      type: "json",
-      fetchOptions: {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      },
+      type: "text",
+    }).then((xml) => {
+      const result = JSON.parse(
+        convert.xml2json(xml, { compact: true, spaces: 2 }),
+      );
+      return result.rss.channel.item;
     });
+
+    const result = feed.map((item) => {
+      const imageURL = item.description._cdata.split('"')[1];
+      return {
+        title: item["letterboxd:filmTitle"]._text,
+        year: item["letterboxd:filmYear"]._text,
+        url: item.link._text,
+        rating: item["letterboxd:memberRating"]._text,
+        poster: imageURL,
+      };
+    });
+
+    return result;
   }
 }
-
